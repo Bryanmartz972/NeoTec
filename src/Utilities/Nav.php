6a4cb387b8a70ae3eb7727787b77b34fc5ed5cf6
Bryan Martínez <bryanmartz972@gmail.com>
2021-11-30 00:12:55 -0600

    init commit

---
+++ src/Utilities/Nav.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Utilities;
+
+class Nav {
+
+    public static function setNavContext(){
+        $tmpNAVIGATION = array();
+        $userID = \Utilities\Security::getUserId();
+        if (\Utilities\Security::isAuthorized($userID, "MntUsuarios")) {
+            $tmpNAVIGATION[] = array(
+                "nav_url" => "index.php?page=mnt_usuarios",
+                "nav_label" => "Usuarios"
+            );
+        }
+        if (\Utilities\Security::isAuthorized($userID, "WW_Scores")) {
+            $tmpNAVIGATION[] = array(
+                "nav_url" => "index.php?page=scores_scores",
+                "nav_label" => "Partituras"
+            );
+        }
+       
+        \Utilities\Context::setContext("NAVIGATION", $tmpNAVIGATION);
+    }
+
+
+    private function __construct()
+    {
+        
+    }
+    private function __clone()
+    {
+        
+    }
+}
+?>
